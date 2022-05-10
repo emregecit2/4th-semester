df1674bcc27037f241453b25f5d037ad734aaec8
emregecit2 <emregecit2@gmail.com>
2022-05-10 22:00:38 +0300

    commit msg

--- ceng232/hw3/lab3_part1/lab3_1.v
+++ ceng232/hw3/lab3_part1/lab3_1.v
@@ -2,10 +2,14 @@
 
 module ab(input A, input B, input clk, output reg Q);
 
-    initial Q = 0;
-
-    // You can implement your code here
-    // ...
+ initial Q = 0;
+ 
+always@(posedge clk)
+begin
+if (A == 1 && B == 1) Q = !Q;
+else if (A == 1 && B == 0) Q = 0;
+else if (A == 0 && B == 1) Q = 1;
+end
 
 endmodule
 
@@ -18,8 +22,15 @@ module ic1337(// Inputs
               output Q0,
               output Q1,
               output Z);
-
-    // You can implement your code here
-    // ...
-
+				  
+reg A1;
+reg B2;
+always@(*)
+begin
+A1 <= !(! I1 || I0) && !I2;
+B2 <= !(!(I2 || !I1) ^^ I0);
+end
+ab ab1(A1, I2, clk, Q0);
+ab ab2(!I2, B2, clk, Q1);
+assign Z = Q0 ^^ Q1;
 endmodule
