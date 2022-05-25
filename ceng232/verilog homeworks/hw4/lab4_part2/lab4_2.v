7e4b11bf7bf74da20bc38fa11c616bf44f8dcd3d
emregecit2 <emregecit2@gmail.com>
2022-05-25 16:05:48 +0300

    commit

--- ceng232/verilog homeworks/hw4/lab4_part2/lab4_2.v
+++ ceng232/verilog homeworks/hw4/lab4_part2/lab4_2.v
@@ -20,7 +20,7 @@ module lab4_2(// INPUTS
     reg[9:0] current_result;
     reg[2:0]instruction_cache[0:31];
     reg[4:0]value_cache[0:31];
-    reg[4:0] cache_size, cache_index, i, count, v;
+    reg[4:0] cache_size, cache_index, i, count, v, temp;
     initial begin
         cache_size = 0;
         cache_index = 0;
@@ -33,58 +33,72 @@ module lab4_2(// INPUTS
     //      LOGIC     //
     //================//
     //Modify the lines below to implement your design
-    always @(posedge clk) begin
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            cache_size = 0;
+            cache_index = 0;
+            previous_result = 0;
+            current_result = 0;
+            invalidOp = 0;
+            overflow = 0;
+            cacheFull = 0;
+        end
         case (mode)
             0: begin
                 case (opCode)
-                    3'b011: begin
-                        invalidOp = 1;
-                    end
-                    3'b111: begin 
-                        invalidOp = 1;
-                    end
+                    3'b011: invalidOp = 1;
+                    3'b111: invalidOp = 1;
                     default: begin
-                        instruction_cache[cache_size] = opCode;
-                        value_cache[cache_size] = value;
-                        cache_size = cache_size + 1;
-                        invalidOp = 0;
+                        if (!cacheFull) begin
+                            instruction_cache[cache_size] = opCode;
+                            value_cache[cache_size] = value;
+                            cache_size = cache_size + 1;
+                        end
                     end
                 endcase
             end
             1: begin
                 current_result = result;
                 case (instruction_cache[cache_index])
-                    3'b000: begin // ADD
-                        result = result + value_cache[cache_index];
+                    3'b000: begin 
+                        temp = result + value_cache[cache_index];
+                        if (temp > 2**10 - 1) overflow = 1;
+                        else overflow = 0;
+                        result = temp;
                     end
-                    3'b001: begin // ADD2
-                        result = result + previous_result + value_cache[cache_index];
+                    3'b001: begin
+                        temp = result + previous_result + value_cache[cache_index];
+                        if (temp > 2**10 - 1) overflow = 1;
+                        else overflow = 0;
+                        result = temp;
                     end
-                    3'b010: begin // FMA
-                        result = result * previous_result + value_cache[cache_index];
+                    3'b010: begin
+                        temp = result * previous_result + value_cache[cache_index];
+                        if (temp > 2**10 - 1) overflow = 1;
+                        else overflow = 0;
+                        result = temp;
                     end
                     3'b100: begin // POPC
                         count = 0;
                         i = 0;
-                        for (i = 0; i <= 9; i = i + 1) begin
-                            count = count + result[i];
-                        end
+                        for (i = 0; i <= 9; i = i + 1) count = count + result[i];
                         result = count;
+                        overflow = 0;
                     end
-                    3'b101: begin // BREV
+                    3'b101: begin
                         result = ~result;
+                        overflow = 0;
                     end
-                    3'b110: begin // SETR
+                    3'b110: begin
                         v = value_cache[cache_index];
+                        overflow = 0;
                     end
                 endcase
                 cache_index = cache_index + 1;
-                if (cache_index == cache_size) begin
-                    cache_index = v;
-                end
+                if (cache_index == cache_size) cache_index = v;
                 previous_result = current_result;
             end
         endcase
-        previous_result = result;
+        cacheFull = cache_size == 32;    
     end
 endmodule
